2ecebfd692f3905cd6bffbf8237e624d179e2910
Chris Prener <christopher.prener@pfizer.com>
2024-07-09 09:07:37 -0400

    shift to subfunctions

--- R/zi_load_labels.R
+++ R/zi_load_labels.R
@@ -1,46 +1,10 @@
-#' Load List of Available Label Datasets
-#'
-#' @description This function loads a list of available label data sets that can
-#'    be used to label ZIP Codes. Currently, only three-digit ZIP Codes are
-#'    supported.
-#'
-#' @param type A character scalar specifying the type of label data to load. The
-#'   only supported type is  \code{'zip3'} (three-digit ZIP Codes).
-#'
-#' @return A tibble containing date values that can be used with \code{zi_load_labels}.
-#'
-#' @examples
-#' \donttest{
-#'   zi_load_labels_list(type = "zip3")
-#' }
-#'
-#' @export
-zi_load_labels_list <- function(type = "zip3"){
-
-  # check inputs
-  if (!type %in% c("zip3")){
-    stop("The only 'type' currently supported is 'zip3'.")
-  }
-
-  # create output
-  ## load list of available label data sets
-  labels_list <- read.csv(file = "https://raw.githubusercontent.com/chris-prener/usps-zip-ref/main/data/meta.csv")
-
-  ## subset
-  out <- subset(labels_list, type == type)
-
-  ## convert to tibble
-  out <- tibble::as_tibble(out)
-
-  # return output
-  return(out)
-
-}
-
 #' Load Label Data
 #'
 #' @description This function loads a specific label data set that can be used to
 #'     label three-digit ZIP codes in a data frame.
+#'
+#' @usage zi_load_labels(source = "UDS", type = "zip5", include_scf = FALSE,
+#'     vintage = 2022)
 #'
 #' @param source A required character scalar; specifying the source of the label
 #'     data. The only supported sources are \code{'UDS'} (default) and
@@ -48,6 +12,9 @@ zi_load_labels_list <- function(type = "zip3"){
 #' @param type A required character scalar; one of either  \code{'zip3'} or
 #'     \code{'zip5'}. The  \code{'zip3'} type is only available from the 'USPS'
 #'     source, while the \code{'zip5'} type is available from \code{'UDS'}.
+#' @param include_scf A logical scalar required when \code{source = 'USPS'} and
+#'     \code{type = 'zip3'}; specifying whether to include the SCF (Sectional
+#'     Center Facility) ID in the output. The default is \code{FALSE}.
 #' @param vintage A required character or numeric scalar; specifying the date
 #'     for \code{source = 'USPS'} or the year of the data for \code{source = 'UDS'}.
 #'     The \code{zip_load_labels_list()} function can be used to see available
@@ -63,7 +30,8 @@ zi_load_labels_list <- function(type = "zip3"){
 #' }
 #'
 #' @export
-zi_load_labels <- function(source = "UDS", type = "zip5", vintage = 2022){
+zi_load_labels <- function(source = "UDS", type = "zip5", include_scf = FALSE,
+                           vintage = 2022){
 
   # check inputs
   if (!source %in% c("USPS", "UDS")){
@@ -90,6 +58,10 @@ zi_load_labels <- function(source = "UDS", type = "zip5", vintage = 2022){
       stop("The 'UDS' source only provides ZIP5 data between 2009 and 2022.")
     }
 
+    if (include_scf == TRUE){
+      warning("The 'include_scf' argument only modifies the output of 'zip3' labels.")
+    }
+
   } else if (source == "USPS"){
 
     if (type == "zip5"){
@@ -114,20 +86,59 @@ zi_load_labels <- function(source = "UDS", type = "zip5", vintage = 2022){
 
   # load data
   if (source == "UDS"){
-    out <- zi_load_uds(year = vintage_num)
-    out <- dplyr::select(out, zip5 = ZIP, label_city = PO_NAME, label_state = STATE,
-                         label_type = ZIP_TYPE)
-  } else if (source == "USPS" & type == "zip3") {
+    out <- zi_load_labels_uds(year = vintage_num)
+  } else if (source == "USPS") {
+    out <- zi_load_labels_usps(
+      type = type,
+      include_scf = include_scf,
+      vintage = vintage_chr
+    )
+  }
+
+  # return output
+  return(out)
+
+}
+
+# sub functions for zi_load_labels
+
+zi_load_labels_uds <- function(year){
+
+  # load UDS crosswalk
+  out <- zi_load_uds(year = year)
+
+  # format output
+  out <- dplyr::select(out, zip5 = ZIP, label_city = PO_NAME, label_state = STATE,
+                       label_type = ZIP_TYPE)
+
+  # return output
+  return(out)
+
+}
+
+zi_load_labels_usps <- function(type, include_scf, vintage){
+
+  # load USPS crosswalk
+  if (type == "zip3"){
+
+    ## read from GitHub
     out <- readr::read_csv(
-      paste0("https://raw.githubusercontent.com/chris-prener/usps-zip-ref/main/data/zip3_", vintage_chr, ".csv"),
+      paste0("https://raw.githubusercontent.com/chris-prener/usps-zip-ref/main/data/zip3_", vintage, ".csv"),
       col_types = readr::cols(
         zip3 = readr::col_character(),
         scf_id = readr::col_character()
       )
     )
 
+    ## rename cols
     out <- dplyr::rename(out, label_area = destination_area, label_state = destination_state)
 
+    ## optionally remove scf cols
+    if (include_scf == FALSE){
+      out <- subset(out, select = -c(scf_id, scf_name, scf_state))
+    }
+
+    ## format output
     out <- tibble::as_tibble(out)
   }
 
